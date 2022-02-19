e39d1085891cb5ec00c29122d0969ecae1932adf
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-02-19 10:42:37 +0400

    Ajout du style selon l'etat des todos

--- components/allTodos.js
+++ components/allTodos.js
@@ -1,8 +1,12 @@
 import CloseIcon from "@mui/icons-material/Close";
 import DoneIcon from "@mui/icons-material/Done";
+import { useState } from "react";
 const axios = require("axios");
 
 const AllTodos = ({ allTodos, setAllTodos }) => {
+  // const [allTodosFront, setAllTodosFront] = useState()
+  const [errorMessage, setErrorMessage] = useState("");
+
   // header avec le JWT pour les requête vers l'api
   const headersConfig = {
     headers: {
@@ -20,8 +24,45 @@ const AllTodos = ({ allTodos, setAllTodos }) => {
     );
 
     const data = await res;
+  };
 
-    console.log(data);
+  //   fonction qui update la todo et prend en para l'id de la todo et son etat (false or true)
+  const updateTodo = async (todoId, doneState) => {
+    //   update la todo directement du front pour la rapidité car sinon besoin de re récupérer toutes les todos etc ...
+    // om map dans toutes les todos, on trouve la todo a update et on update son state avec la state inverse
+    console.log(doneState);
+    console.log("id: ", todoId);
+    setAllTodos(
+      allTodos.map((x) => {
+        console.log(x);
+        if (x._id != todoId) {
+          console.log("je ne dois pas update la todo", x.title);
+        }
+        if (x._id === todoId) {
+          return { ...x, done: doneState };
+        }
+        return x;
+      })
+    );
+    const res = await axios.put(
+      `http://localhost:3000/api/todos/${todoId}`,
+      {
+        doneState: doneState, //afin d'update l'etat, on envoit l'etat inverse (si true, on renvoit false et inversement)
+      },
+      headersConfig
+    );
+
+    const data = await res;
+
+    // si erreur avec le JWT
+    if (data.data.message === "JWT error") {
+      setErrorMessage("Error, you must be connected");
+    }
+
+    // si aucune erreur, on met à jour la liste des todos
+    if (!data.data.message) {
+      console.log("data updated");
+    }
   };
 
   return (
@@ -31,10 +72,21 @@ const AllTodos = ({ allTodos, setAllTodos }) => {
           <h2 className="text-violet-700 font-bold mb-5">TODOS</h2>
           {allTodos.map((x) => (
             <ul key={x._id} className="flex flex-col">
-              <li className="border-2 p-3 border-violet-700 mb-2 flex justify-between text-violet-700">
+              <li
+                className={`${"border-2 p-3 border-violet-700 mb-2 flex justify-between text-violet-700"} ${
+                  x.done ? "line-through" : "no-underline"
+                }`}
+              >
                 {x.title}
                 <div>
-                  <DoneIcon className="border border-violet-700 text-violet-700 cursor-pointer transition ease-out mr-2 hover:scale-110" />
+                  <DoneIcon
+                    className={`${"mr-2 cursor-pointer"} ${
+                      x.done
+                        ? "bg-violet-700 text-white" //si done == true
+                        : "border border-violet-700 text-violet-700" //si donne == false
+                    }`}
+                    onClick={() => updateTodo(x._id, !x.done)}
+                  />
                   <CloseIcon
                     className="border border-red-500 text-red-500 cursor-pointer transition ease-out hover:bg-red-500 hover:text-white"
                     onClick={() => deleteTodo(x._id)}
@@ -45,6 +97,7 @@ const AllTodos = ({ allTodos, setAllTodos }) => {
           ))}
         </div>
       )}
+      {errorMessage && <p>{errorMessage}</p>}
     </div>
   );
 };
--- pages/api/todos/[todoId].js
+++ pages/api/todos/[todoId].js
@@ -8,6 +8,9 @@ export default async function handler(req, res) {
   // connexion a MongoDB
   const { db } = await connectToDatabase();
 
+  // middleware cors
+  await nextCors(req, res);
+
   //   recupere le JWT
   const token = req.headers["x-access-token"];
 
@@ -19,25 +22,43 @@ export default async function handler(req, res) {
       process.env.ACCESS_TOKEN_SECRET
     );
 
-    // recupere l'id de la todo a supprimer
-    const { todoId } = req.query;
+    // suppression todo
+    if (req.method === "DELETE") {
+      // recupere l'id de la todo a supprimer
+      const { todoId } = req.query;
 
-    console.log(todoId);
+      console.log(todoId);
 
-    // supprime la todo de la base de donnée
-    const deleteTodo = await db
-      .collection("todos")
-      .deleteOne({ _id: new mongodb.ObjectId(todoId) });
+      // supprime la todo de la base de donnée
+      const deleteTodo = await db
+        .collection("todos")
+        .deleteOne({ _id: new mongodb.ObjectId(todoId) });
 
-    console.log(deleteTodo);
+      console.log(deleteTodo);
 
-    console.log("todo supprimer avec succes");
-    return res.status(200).send({ message: "todo supprimer" });
+      console.log("todo supprimer avec succes");
+      return res.status(200).send({ message: "todo supprimer" });
+    }
+
+    // Mise a jour todo
+    if (req.method === "PUT") {
+      // recupere l'id de la todo
+      const { todoId } = req.query;
+
+      //   recupere l'etat de la todo
+      const { doneState } = req.body;
+
+      //   udpate la todo
+      const updateTodo = await db.collection("todos").updateOne(
+        { _id: new mongodb.ObjectId(todoId) }, //filtre
+        { $set: { done: doneState } } //$set -> atomic operators mongodb afin d'update
+      );
+
+      console.log("todo update avec succes");
+      return res.status(200).send({ message: "update reussi" });
+    }
   } catch (err) {
     console.log(err);
     return res.send({ message: "JWT error" });
   }
-
-  // middleware cors
-  await nextCors(req, res);
 }
